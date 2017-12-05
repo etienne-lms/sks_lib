699b869aef31bb79fa0bec99f48c8a6729bf8533
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-05 14:11:41 +0100

    add local utilitaries
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ lib/local_utils.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef __LOCAL_UTILS_H
+#define __LOCAL_UTILS_H
+
+#include <stdio.h>
+
+#define ASSERT(cond) \
+	do { \
+		if (!(cond)) { \
+			LOG_ERROR("Assert failed in %s, %s:%d", \
+				  __func__, __FILE__, __LINE__); \
+			while (1) \
+				; \
+		} \
+	} while (0)
+
+#define LOG_ERROR(...)	printf(__VA_ARGS__)
+#define LOG_INFO(...)	printf(__VA_ARGS__)
+#define LOG_DEBUG(...)	printf(__VA_ARGS__)
+
+#endif /*__LOCAL_UTILS_H*/
