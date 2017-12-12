ac48661fb46eb64c579981bec2ca5b0673248ed8
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-12 17:20:24 +0100

    debug: stringuify mechanism ids and some flags
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- lib/include/sks_ck_debug.h
+++ lib/include/sks_ck_debug.h
@@ -11,7 +11,15 @@
 /* Return a pointer to a string buffer of "CKA_xxx\0" attribute ID */
 const char *cka2str(CK_ATTRIBUTE_TYPE id);
 
-/* Return a pointer to a string buffer of "CKR_xxx\0" attribute ID */
+/* Return a pointer to a string buffer of "CKR_xxx\0" return value ID */
 const char *ckr2str(CK_RV id);
+
+/* Return a pointer to a string buffer of "CKM_xxx\0" mechanism ID */
+const char *ckm2str(CK_MECHANISM_TYPE id);
+
+/* Allocate and return a string descripbing the enabled flags */
+char *ck_slot_flag2str(CK_ULONG flags);
+char *ck_token_flag2str(CK_ULONG flags);
+char *ck_mecha_flag2str(CK_ULONG flags);
 
 #endif /*__SKS_CK_DEBUG_H*/
